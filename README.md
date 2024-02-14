8d3db01d1a8f7d98e4fd263f7e9d37794a219ffe
Matthias Greshake <mgreshake@uni-osnabrueck.de>
2024-02-14 19:50:25 +0100

    restructured star catalogue

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ parameters are freely configurable in conformity with the laws of physics.
 
 ## Getting Started
 
-Use these instructions to get a copy of the project running in a virtual 
+Follow these instructions to get a copy of the project running in a virtual
 environment on your local machine.
 
 ### Prerequisites
@@ -35,6 +35,29 @@ corresponding name. The available systems are listed in `star_catalogue.yml`.
 Define your own parameterization by adding new star systems to the catalogue. 
 Note that certain combinations of parameters lead to physical unstable 
 solutions and therefore cannot be simulated.
+
+### Parameters
+
+The table below lists all configurable parameters including their units. If you
+omit a parameter, the default value is used instead.
+
+|    Group    |          Parameter          |          Unit          | Default |
+|-------------|-----------------------------|------------------------|--------:|
+| all         | eccentricity                |                        |     0.0 |
+|             | longitude of ascending node | degree                 |     0.0 |
+|             | inclination                 | degree                 |    90.0 |
+|             | argument of periapsis       | degree                 |     0.0 |
+| binary star | mass                        | solar mass             |     1.0 |
+|             | radius                      | solar radius           |     1.0 |
+|             | luminosity                  | solar luminosity       |     1.0 |
+|             | temperature                 | Kelvin                 |    5780 |
+|             | semi-major axis             | astronomical unit (AU) |     0.2 |
+| planet      | mass                        | Jupiter mass           |     1.0 |
+|             | radius                      | Jupiter radius         |     1.0 |
+|             | semi-major axis             | astronomical unit (AU) |     1.0 |
+| satellite   | mass                        | earth mass             |     0.1 |
+|             | radius                      | earth radius           |     0.5 |
+|             | semi-major axis             | astronomical unit (AU) |    0.01 |
 
 ## License
 
--- utils/parameter_config.py
+++ utils/parameter_config.py
@@ -4,6 +4,7 @@ import yaml
 class ParameterConfig:
     """Container that holds parameters for a binary star system.
     """
+
     def __init__(self, star_system, config_path="star_catalogue.yml"):
         """Reads parameters from configuration file.
 
@@ -15,37 +16,51 @@ class ParameterConfig:
 
         config = content[star_system]
 
-        self.mass_stars_in_sun = config.get('mass stars', [1.0, 1.0])
-        self.radius_stars_in_sun = config.get('radius stars', [1.0, 1.0])
-        self.luminosity_stars_in_sun = config.get('luminosity stars', [1.0, 1.0])
-        self.temperature_stars_in_kelvin = config.get('temperature stars', [5780, 5780])
-        self.semi_major_axis_binary_star_in_au = config.get('semi-major axis stars', 0.2)
-        self.eccentricity_binary_star = config.get('eccentricity stars', 0.0)
-        self.longitude_of_ascending_node_binary_star_in_deg = config.get('longitude of ascending node stars', 0.0)
-        self.inclination_binary_star_in_deg = config.get('inclination stars', 90.0)
-        self.argument_of_periapsis_binary_star_in_deg = config.get('argument of periapsis stars', 0.0)
+        self.semi_major_axis_binary_star_in_au = config.get('semi-major axis', 0.2)
+        self.eccentricity_binary_star = config.get('eccentricity', 0.0)
+        self.longitude_of_ascending_node_binary_star_in_deg = config.get('longitude of ascending node', 0.0)
+        self.inclination_binary_star_in_deg = config.get('inclination', 90.0)
+        self.argument_of_periapsis_binary_star_in_deg = config.get('argument of periapsis', 0.0)
 
-        self.semi_major_axis_planets_in_au = config.get('semi-major axis planets', [])
-        planets = range(len(self.semi_major_axis_planets_in_au))
-        self.mass_planets_in_jupiter = config.get('mass planets', [1.0 for _ in planets])
-        self.radius_planets_in_jupiter = config.get('radius planets', [1.0 for _ in planets])
-        self.eccentricity_planets = config.get('eccentricity planets', [0.0 for _ in planets])
-        self.longitude_of_ascending_node_planets_in_deg = config.get(
-            'longitude of ascending node planets', [0.0 for _ in planets])
-        self.inclination_planets_in_deg = config.get('inclination planets', [90.0 for _ in planets])
-        self.argument_of_periapsis_planets_in_deg = config.get('argument of periapsis planets', [0.0 for _ in planets])
+        stars = config.get('stars', [])
+        self.mass_stars_in_sun = self.aggregate(stars, 'mass', 1.0)
+        self.radius_stars_in_sun = self.aggregate(stars, 'radius', 1.0)
+        self.luminosity_stars_in_sun = self.aggregate(stars, 'luminosity', 1.0)
+        self.temperature_stars_in_kelvin = self.aggregate(stars, 'temperature', 5780)
 
-        self.semi_major_axis_satellites_in_au = config.get('semi-major axis satellites', [[] for _ in planets])
-        satellites = [range(len(self.semi_major_axis_satellites_in_au[p])) for p in planets]
-        self.mass_satellites_in_earth = config.get(
-            'mass satellites', [[0.1 for _ in satellites[p]] for p in planets])
-        self.radius_satellites_in_earth = config.get(
-            'radius satellites', [[0.5 for _ in satellites[p]] for p in planets])
-        self.eccentricity_satellites = config.get(
-            'eccentricity satellites', [[0.0 for _ in satellites[p]] for p in planets])
-        self.longitude_of_ascending_node_satellites_in_deg = config.get(
-            'longitude of ascending node satellites', [[0.0 for _ in satellites[p]] for p in planets])
-        self.inclination_satellites_in_deg = config.get(
-            'inclination satellites', [[90.0 for _ in satellites[p]] for p in planets])
-        self.argument_of_periapsis_satellites_in_deg = config.get(
-            'argument of periapsis satellites', [[0.0 for _ in satellites[p]] for p in planets])
+        planets = config.get('planets', [])
+        self.mass_planets_in_jupiter = self.aggregate(planets, 'mass', 1.0)
+        self.radius_planets_in_jupiter = self.aggregate(planets, 'radius', 1.0)
+        self.semi_major_axis_planets_in_au = self.aggregate(planets, 'semi-major axis', 1.0)
+        self.eccentricity_planets = self.aggregate(planets, 'eccentricity', 0.0)
+        self.longitude_of_ascending_node_planets_in_deg = self.aggregate(planets, 'longitude of ascending node', 0.0)
+        self.inclination_planets_in_deg = self.aggregate(planets, 'inclination', 90.0)
+        self.argument_of_periapsis_planets_in_deg = self.aggregate(planets, 'argument of periapsis', 0.0)
+
+        satellites = [planet.get('satellites', []) for planet in planets]
+        self.mass_satellites_in_earth = self.aggregate(satellites, 'mass', 0.1)
+        self.radius_satellites_in_earth = self.aggregate(satellites, 'radius', 0.5)
+        self.semi_major_axis_satellites_in_au = self.aggregate(satellites, 'semi-major axis', 0.01)
+        self.eccentricity_satellites = self.aggregate(satellites, 'eccentricity', 0.0)
+        self.longitude_of_ascending_node_satellites_in_deg = self.aggregate(
+            satellites, 'longitude of ascending node', 0.0)
+        self.inclination_satellites_in_deg = self.aggregate(satellites, 'inclination', 90.0)
+        self.argument_of_periapsis_satellites_in_deg = self.aggregate(satellites, 'argument of periapsis', 0.0)
+
+    @staticmethod
+    def aggregate(config, key, default):
+        """Searches configuration recursively and aggregates all values that match key.
+
+        :param config:  Configuration to be searched.
+        :param key:     Key to be matched.
+        :param default: Default value if key is not available.
+        """
+        aggregations = []
+
+        for element in config:
+            if isinstance(element, list):
+                aggregations.append(ParameterConfig.aggregate(element, key, default))
+            else:
+                aggregations.append(element.get(key, default))
+
+        return aggregations
